40f4bb2dc0168fb3eb6f6ba1a4d3f5840b723482
LIBBYXDPG3D <99547031+LIBBYXDPG3D@users.noreply.github.com>
2022-03-21 13:57:51 -0400

    Update Pixel Gun 3D SCRIPT Made By LibbyXD PG3D.lua

--- Pixel Gun 3D SCRIPT Made By LibbyXD PG3D.lua
+++ Pixel Gun 3D SCRIPT Made By LibbyXD PG3D.lua
@@ -7,17 +7,17 @@ gg.setVisble(false) end
 ---Function start()
 
 HOME = gg.choice({
-      'HACK GEMS'
-      'HACK COINS'
-      'XP HACK'
-      "EXIT"} nil, ("LibbyXD Plays PG3D ON YT")
+      "HACK GEMS" ,
+      "HACK COINS" ,
+      "XP HACK" ,
+      "EXIT"} , nil ,"LibbyXD Plays PG3D ON YT")
       
 ------function 1()
   
   if HOME == 1 then
   choice1 = gg.choice ({
-    "HACK SILVER"
-    "OTHER", nil, ("HI DEAR HACKER") end
+    "HACK SILVER" ,
+    "OTHER" , nil , "HI DEAR HACKER") end
     if Choice 1 == 1 then
     prompt1 = gg.prompt ({
       "TYPE THE AMOUNT OF COINS FOR GEMS YOU WANT TO HACK"
