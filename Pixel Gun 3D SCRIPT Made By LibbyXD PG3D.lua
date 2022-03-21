a3296f695941f7c9527e470018d36b39d4072d51
LIBBYXDPG3D <99547031+LIBBYXDPG3D@users.noreply.github.com>
2022-03-21 13:44:13 -0400

    Update Pixel Gun 3D SCRIPT Made By LibbyXD PG3D.lua

--- Pixel Gun 3D SCRIPT Made By LibbyXD PG3D.lua
+++ Pixel Gun 3D SCRIPT Made By LibbyXD PG3D.lua
@@ -1,1 +1,24 @@
-Hi So I Am A Youtuber And On Github I'm Am An Undercover User... I need Some PG3D Modders To Help Me If Anyone See This... That's All I Have To Say! Bye See You Soon!
+gg.alert('Welcome User')
+gg.toast('Made Anthony AKA LibbyXD PG3D')
+
+if gg.isVisible(true) then
+gg.setVisble(false) end
+
+---Function start()
+
+HOME = gg.choice({
+      "HACK GEMS"
+      "HACK COINS"
+      "XP HACK"
+      "EXIT"} nil, ("LibbyXD Plays PG3D ON YT")
+      
+------function 1()
+  
+  if HOME == 1 then
+  choice1 = gg.choice ({
+    "HACK SILVER"
+    "OTHER", nil, ("HI DEAR HACKER") end
+    if Choice 1 == 1 then
+    prompt1 = gg.prompt ({
+      "TYPE THE AMOUNT OF COINS FOR GEMS YOU WANT TO HACK"
+    gg.searchNumber (prompt1[1],gg.TYPE_DWORD)
